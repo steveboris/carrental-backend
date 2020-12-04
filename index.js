a654c6b394fbeacb9dfb002aead0b15a64e8ee26
steveboris <b.steve189@hotmail.com>
2020-12-04 14:03:05 +0100

    router for categories

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const database = require('./manager/database');
 const users = require('./routes/users');
 const brands = require('./routes/brands');
 const locations = require('./routes/locations');
+const categories = require('./routes/categories');
+
 
 const conn = new database({
     host: "localhost",
@@ -18,5 +20,6 @@ app.use(express.json());
 app.use("/api/v1/users", users);
 app.use("/api/v1/brands", brands);
 app.use("/api/v1/locations", locations);
+app.use("/api/v1/categories", categories);
 
 app.listen(3000, () => console.log("The server is running and listening on port 3000."));
